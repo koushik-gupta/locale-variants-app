f26fa009cf37356ab06905c713d260222413f423
Koushik Gupta <koushik26gupta@gmail.com>
2025-09-05 01:21:06 +0530

    Fix: Set Contentstack region to EU

--- backend/services/contentstackService.js
+++ backend/services/contentstackService.js
@@ -6,6 +6,7 @@ const Stack = contentstack.Stack({
   api_key: process.env.CONTENTSTACK_API_KEY,
   delivery_token: process.env.CONTENTSTACK_DELIVERY_TOKEN,
   environment: process.env.CONTENTSTACK_ENVIRONMENT,
+  region: "eu",
 });
 
 /**
